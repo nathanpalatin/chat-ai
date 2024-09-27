7f6f9b6e6be6aed6f4171aa517703b8b3d9664c9
Nathan Palatin <nath.palatin@gmail.com>
2024-09-27 14:40:29 -0300

    chore: finalizado chat IA.

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,9 +1,82 @@
 import { NextResponse } from 'next/server'
 
-export async function POST(request: Request) {
-	const { message } = await request.json()
+export async function POST(req: Request) {
+	const { prompt } = await req.json()
 
-	const botResponse = `Opa! Muito prazer ${message}. Em que posso ajudar-lo?`
+	if (!prompt) {
+		return NextResponse.json({ error: 'Prompt is required' }, { status: 400 })
+	}
 
-	return NextResponse.json({ response: botResponse })
+	const apiKey = process.env.OPENAI_API_KEY
+
+	if (!apiKey) {
+		return NextResponse.json({ error: 'API key is missing' }, { status: 500 })
+	}
+
+	try {
+		const response = await fetch('https://api.openai.com/v1/chat/completions', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${apiKey}`
+			},
+			body: JSON.stringify({
+				model: 'gpt-3.5-turbo',
+				messages: [
+					{
+						role: 'system',
+						content:
+							'Aja como eu, Nathan Palatin, programador, especializado em NextJs, NodeJs e React Native, ajude a pessoas a programar.'
+					},
+					{ role: 'user', content: prompt }
+				],
+				stream: true
+			})
+		})
+
+		const readableStream = new ReadableStream({
+			async start(controller) {
+				const decoder = new TextDecoder()
+				const reader = response.body?.getReader()
+
+				if (!reader) {
+					controller.error('Error starting stream')
+					return
+				}
+
+				while (true) {
+					const { done, value } = await reader.read()
+					if (done) break
+
+					const chunk = decoder.decode(value, { stream: true })
+					const lines = chunk.split('\n').filter(line => line.trim() !== '')
+
+					for (const line of lines) {
+						if (line === 'data: [DONE]') {
+							controller.close()
+							return
+						}
+
+						try {
+							const json = JSON.parse(line.replace(/^data: /, ''))
+							const content = json.choices[0]?.delta?.content || ''
+
+							if (content) {
+								controller.enqueue(new TextEncoder().encode(content))
+							}
+						} catch (error) {
+							console.error('Error parsing JSON', error)
+						}
+					}
+				}
+			}
+		})
+
+		return new Response(readableStream, {
+			headers: { 'Content-Type': 'text/event-stream' }
+		})
+	} catch (error) {
+		console.error('Error:', error)
+		return NextResponse.json({ error: 'Failed to fetch from OpenAI API' }, { status: 500 })
+	}
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,30 @@
 'use client'
 
-import { SetStateAction, useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
-import { Skeleton } from '@/components/ui/skeleton'
 import { Send } from 'lucide-react'
 
 export default function Home() {
-	const [messages, setMessages] = useState([
-		{
-			id: 1,
-			role: 'ai',
-			content: 'Olá, boa tarde! qual seu nome?'
-		}
-	])
+	const [messages, setMessages] = useState([{ id: 1, role: 'ai', content: 'Opa, em que posso ser ajudar?' }])
 	const [input, setInput] = useState('')
-	const [typingMessage, setTypingMessage] = useState('')
-	const [isTyping, setIsTyping] = useState(false)
 
-	const handleInputChange = (e: { target: { value: SetStateAction<string> } }) => {
+	const handleInputChange = (e: { target: { value: string } }) => {
 		setInput(e.target.value)
 	}
+
+	const messagesEndRef = useRef<null | HTMLDivElement>(null)
+
+	const scrollToBottom = () => {
+		messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' })
+	}
+
+	useEffect(() => {
+		scrollToBottom()
+	}, [messages])
 
 	const handleSubmit = async (e: { preventDefault: () => void }) => {
 		e.preventDefault()
@@ -33,93 +34,83 @@ export default function Home() {
 		const userMessage = {
 			id: messages.length + 1,
 			role: 'user',
-			content: input,
+			content: input
 		}
 
-		setMessages((prevMessages) => [...prevMessages, userMessage])
+		setMessages(prevMessages => [...prevMessages, userMessage])
 		setInput('')
 
 		try {
-			const response = await fetch('/api/chat', {
+			const res = await fetch('/api/chat', {
 				method: 'POST',
 				headers: {
-					'Content-Type': 'application/json',
+					'Content-Type': 'application/json'
 				},
-				body: JSON.stringify({ message: input }),
+				body: JSON.stringify({ prompt: input })
 			})
 
-			const data = await response.json()
-			setTypingMessage(data.response)
-			setIsTyping(true)
+			if (!res.ok) {
+				throw new Error('Failed to fetch from the API')
+			}
 
-			setTimeout(() => {
-				setMessages((prevMessages) => [...prevMessages, { id: prevMessages.length + 1, role: 'ai', content: 'aeeee' }])
-			}, 500)
+			const reader = res.body?.getReader()
+			const decoder = new TextDecoder()
 
+			let aiMessage = { id: messages.length + 2, role: 'ai', content: '' }
+			setMessages(prevMessages => [...prevMessages, aiMessage])
+
+			while (true) {
+				const { done, value } = await reader?.read()!
+
+				if (done) break
+
+				const chunk = decoder.decode(value, { stream: true })
+				aiMessage.content += chunk
+
+				setMessages(prevMessages => {
+					const updatedMessages = [...prevMessages]
+					updatedMessages[updatedMessages.length - 1] = aiMessage
+					return updatedMessages
+				})
+			}
 		} catch (error) {
 			console.error('Error:', error)
+			setTimeout(() => {
+				setMessages(prevMessages => [
+					...prevMessages,
+					{ id: prevMessages.length + 1, role: 'ai', content: 'Erro na resposta' }
+				])
+			}, 3000)
 		}
 	}
 
-	useEffect(() => {
-		if (!isTyping) return
-
-		const typingInterval = setInterval(() => {
-			setMessages((prevMessages) => {
-				const lastMessage = prevMessages[prevMessages.length - 1]
-				if (lastMessage && lastMessage.role === 'ai') {
-					if (lastMessage.content.length < typingMessage.length) {
-						const newContent = typingMessage.slice(0, lastMessage.content.length + 1)
-						return [
-							...prevMessages.slice(0, -1),
-							{ ...lastMessage, content: newContent }
-						]
-					} else {
-						clearInterval(typingInterval)
-						setIsTyping(false)
-					}
-				}
-				return prevMessages
-			})
-		}, 70)
-
-		return () => clearInterval(typingInterval)
-	}, [isTyping, typingMessage])
-
 	return (
 		<div className="flex flex-col gap-2 items-center justify-center bg-zinc-950 min-h-screen">
-			<Card className="w-[440px] bg-zinc-900 border-none">
-				<CardHeader className='border-b border-zinc-600/20'>
-					<CardTitle className="text-white">Chat by Neitan</CardTitle>
-					<CardDescription>Converse com a inteligência artificial sobre a vida.</CardDescription>
+			<Card className="w-[500px] bg-zinc-900 border-none">
+				<CardHeader className="border-b border-zinc-600/20">
+					<CardTitle className="text-white">Nathan, we have a problem.</CardTitle>
+					<CardDescription>Ask me anything...</CardDescription>
 				</CardHeader>
 				<CardContent>
-					<ScrollArea className="h-[600px] w-full space-y-4">
-						{messages.map(m => (
-							<div key={m.id} className="whitespace-pre-wrap">
-								{m.role === 'user' ? (
-									<div className="flex gap-3 space-y-2 text-slate-300 text-sm">
-										<Avatar>
-											<AvatarImage src='/avatar.jpeg' />
-											<AvatarFallback><Skeleton className='w-full h-full bg-zinc-500' /></AvatarFallback>
-										</Avatar>
-										<p className="leading-relaxed">
-											<span className="block font-bold text-slate-100">Nathan:</span>
-											{m.content}
-										</p>
-									</div>
-								) : (
-									<div className="flex gap-3 text-slate-300 text-sm">
-										<Avatar>
-											<AvatarImage src="https://github.com/rocketseat.png" />
-											<AvatarFallback>Houston:</AvatarFallback>
-										</Avatar>
-										<p className="leading-relaxed">
-											<span className="block font-bold text-slate-100">Houston:</span>
-											{m.content}
-										</p>
-									</div>
+					<ScrollArea ref={messagesEndRef} className="h-[600px] w-full space-y-4 pr-6">
+						{messages.map(message => (
+							<div
+								key={message.id}
+								className={`flex mb-4 ${message.role === 'user' ? 'justify-end' : 'justify-start'}`}
+							>
+								{message.role === 'ai' && (
+									<Avatar className="mr-2">
+										<AvatarImage src="https://github.com/nathanpalatin.png" alt="AI Avatar" />
+										<AvatarFallback>AI</AvatarFallback>
+									</Avatar>
 								)}
+								<div
+									className={`py-2 px-4 rounded-lg text-sm ${
+										message.role === 'user' ? 'bg-zinc-500 text-white' : 'bg-gray-700 text-white'
+									}`}
+								>
+									{message.content}
+								</div>
 							</div>
 						))}
 					</ScrollArea>
@@ -138,7 +129,6 @@ export default function Home() {
 					</form>
 				</CardFooter>
 			</Card>
-			<p className='text-xs text-zinc-800'>Powered by Nathan Palatin &copy; 2024</p>
 		</div>
 	)
 }
